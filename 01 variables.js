ec59d69662b667cd26edadba9b15871a11f1a937
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-20 09:19:05 +0200

    first commit

---
+++ 01 variables.js
@@ -0,0 +1,21 @@
+// ZMIENNE
+
+let a = 2
+let b = 4
+let c = 3
+let d = 2
+console.log(a + (b * c) / d)
+
+// Zadanie
+
+const city = 'Kraków';
+const street = "ul. Zielona"
+const streetNumber = '10/13'
+const name = 'Joanna'
+const surname = 'Popala'
+
+
+console.log(
+	'Adres dostawy paczki: ' + city + ',' + ' ' + street + ' ' + streetNumber + '.' + ' Nadawca: ' + name + ' ' + surname
+)
+console.log(`Adres dostawy paczki: ${city}, ${street} ${streetNumber}. Nadawca: ${name} ${surname}`) // zapis przy pomocy backticka
