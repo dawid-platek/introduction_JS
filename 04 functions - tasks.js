5ac7f1b978c6b9fe2ef5dcb347f0a7953ba18de4
dawidplatek <dawidplatek.eu.pl@gmail.com>
2025-01-02 23:06:23 +0100

    04 functions - tasks (part II)

--- 04 functions - tasks.js
+++ 04 functions - tasks.js
@@ -1,4 +1,4 @@
-//Funckje - zadania -> do wpisania do konsoli przeglądarki    
+//Funkcje - zadania -> do wpisania do konsoli przeglądarki    
     // Zadanie I
         // I sposób
 function showDeliveryAddress(city, street, streetNumber) {
@@ -13,3 +13,30 @@ showDeliveryAddress('Warsaw', 'Marszałkowska', 10);
             document.write(text);
           }
           showDeliveryAddress('Warsaw', 'Marszałkowska', 10);
+
+
+
+    // Zadanie II
+function add(a,b) {return a + b}
+function divide(a,b) {return a / b} 
+function multiply(a,b) {return a * b}
+
+const multiplyResult = multiply(2, 3);
+const divideResult = divide(6, 2);
+const result = add(multiplyResult, divideResult);
+
+function showResult(number) {
+  document.write(`Result: ${number}`);
+  }
+
+
+    // Zadanie III
+const add = (a,b) => a + b;
+const divide = (a,b) => a / b 
+const multiply = (a,b) => a * b
+
+const multiplyResult = multiply(2, 3);
+const divideResult = divide(6, 2);
+const result = add(multiplyResult, divideResult);
+
+const showResult = number => document.write(`Result: ${number}`);
